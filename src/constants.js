77bde67260ce73863696b68393e4d44237cedffb
Jason Davis <jason24@hey.com>
2021-11-15 11:30:26 -0500

    building out the rest of the game from the tutorial

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x0d1e217Fd9265CBe1A1DBe2AB442DE27f920c288';
+
+const transformCharacterData = (characterData) => {
+    return {
+        name: characterData.name,
+        imageURI: characterData.imageURI,
+        hp: characterData.hp.toNumber(),
+        maxHp: characterData.maxHp.toNumber(),
+        attackDamage: characterData.attackDamage.toNumber(),
+    };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
